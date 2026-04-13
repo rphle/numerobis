d006ae92269a9d3281762d055e0801a614a8f7d6
rphle <hello@alpharee.de>
2026-04-13 22:43:30 +0200

    Improve formatting in newly added audio bindings

--- runtime/numerobis/builtins/graphics/graphics_builtins.c
+++ runtime/numerobis/builtins/graphics/graphics_builtins.c
@@ -388,41 +388,37 @@ static Value numerobis_builtin_set_origin(Value *args) {
 }
 
 void audio_callback(void *userdata, Uint8 *stream, int len) {
-	if ( audio_len == 0 ) {
+  if (audio_len == 0) {
     return;
   }
-	
-	len = ( len > audio_len ? audio_len : len );
-	SDL_memcpy (stream, audio_pos, len); // simply copy from one buffer into the other
-	// SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME); // mix from one buffer into another
-	
-	audio_pos += len;
-	audio_len -= len;
+  len = (len > audio_len ? audio_len : len);
+  SDL_memcpy(stream, audio_pos, len);
+  audio_pos += len;
+  audio_len -= len;
 }
 
 int audio_wait_thread(void *arg) {
-	SDL_LoadWAV(arg, &wav_spec, &wav_buffer, &wav_length);
+  SDL_LoadWAV(arg, &wav_spec, &wav_buffer, &wav_length);
 
-	wav_spec.callback = audio_callback;
-	wav_spec.userdata = NULL;
-	// set our global static variables
-	audio_pos = wav_buffer; // copy sound buffer
-	audio_len = wav_length; // copy file length
-	
-	/* Open the audio device */
-	if ( SDL_OpenAudio(&wav_spec, NULL) < 0 ){
-	  fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
-	  exit(-1);
-	}
+  wav_spec.callback = audio_callback;
+  wav_spec.userdata = NULL;
+  audio_pos = wav_buffer;
+  audio_len = wav_length;
 
-	SDL_PauseAudio(0);
+  /* Open the audio device */
+  if (SDL_OpenAudio(&wav_spec, NULL) < 0) {
+    fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
+    exit(-1);
+  }
+
+  SDL_PauseAudio(0);
   while (audio_len > 0) {
     SDL_Delay(25);
   }
 
-	// shut everything down
-	SDL_CloseAudio();
-	SDL_FreeWAV(wav_buffer);
+  /* Shut everything down */
+  SDL_CloseAudio();
+  SDL_FreeWAV(wav_buffer);
 
   return 1;
 }
@@ -431,8 +427,8 @@ static Value numerobis_builtin_play_sound(Value *args) {
   sds path = _str(args[1]);
   audio_len = 0;
   SDL_Delay(60);
-	
-	/* Start playing */
+
+  /* Start playing */
   SDL_Thread *thread = SDL_CreateThread(audio_wait_thread, "audio_wait", path);
   SDL_DetachThread(thread);
 
